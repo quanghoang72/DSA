b94747e711d4e4f5cb563fad625231359b7086df
quanghoang72 <49017351+quanghoang72@users.noreply.github.com>
2022-12-20 21:17:46 +0200

    Time: 3 ms (34.89%), Space: 6.1 MB (39.37%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int first=1;
+        int second=2;
+        int res=0;
+        for(int i=3;i<=n;i++){
+            res=first+second;
+            first=second;
+            second=res;
+        }
+        return n <=2 ? n : res;
+
+    }
+};
