6513711ab51731e911318f7f1f5cdf7053c495bd
quanghoang72 <49017351+quanghoang72@users.noreply.github.com>
2022-12-20 22:59:30 +0200

    Time: 13 ms (28.24%), Space: 7.7 MB (89.58%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res(n+1);
+        res[0]=0;
+        for(int i=1;i<=n;i++){
+            if(i&1) res[i]=res[i-1]+1;
+            else res[i]=res[i/2];
+        }
+    return res;
+    }
+};
