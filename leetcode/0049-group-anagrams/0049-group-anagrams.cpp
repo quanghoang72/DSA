225fd878d24d42c88b7ae917fba77ce7446b504f
quanghoang72 <SUPER_SECRET>
2023-01-06 21:45:40 +0000

    add to lc

--- leetcode/0049-group-anagrams/0049-group-anagrams.cpp
+++ leetcode/0049-group-anagrams/0049-group-anagrams.cpp
@@ -5,14 +5,33 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& a) {
         vector<vector<string>> res;
         unordered_map<string,vector<string>> mp;
+        // for(int i=0;i<a.size();i++){
+        //     string temp=a[i];
+        //     sort(temp.begin(),temp.end());
+        //     mp[temp].push_back(a[i]);
+        // }
+        // for(auto x : mp){
+        //     res.push_back(x.second);
+        // }
+        // return res;
         for(int i=0;i<a.size();i++){
-            string temp=a[i];
-            sort(temp.begin(),temp.end());
-            mp[temp].push_back(a[i]);
+            string key=sortstring(a[i]);
+            mp[key].push_back(a[i]);
         }
         for(auto x : mp){
             res.push_back(x.second);
         }
         return res;
+    }
+    string sortstring(string s){
+        vector<int> alpha(26);
+        for(int i=0;i<s.size();i++){
+            alpha[s[i]-'a']++;
+        }
+        string ret;
+        for(int i=0;i<26;i++){
+            ret.append(to_string(alpha[i]+'a'));
+        }
+        return ret;
     }
 };
