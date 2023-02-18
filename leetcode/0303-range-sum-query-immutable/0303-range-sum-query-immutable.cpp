05070c6bfcca4e5f2ce3369dfffa6a064b8b98f4
quanghoang72 <SUPER_SECRET>
2023-02-18 17:47:32 +0000

    add to lc

--- leetcode/0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ leetcode/0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,18 +1,20 @@
 class NumArray {
 public:
-     NumArray(vector<int> &nums) : psum(nums.size()+1, 0) {
-        partial_sum( nums.begin(), nums.end(), psum.begin()+1);
+    NumArray(vector<int>& a) {
+        v=vector<int>(a.size()+1,0);
+        partial_sum(a.begin(),a.end(),v.begin()+1); //accumulate prefix subarray assign to new array at index 1
     }
     
-    int sumRange(int i, int j) {
-        return psum[j+1] - psum[i];
+    int sumRange(int left, int right) {
+        //desire range= accumulate sum to right position minus-the prefix sum subarray position before left
+        return v[right+1] - v[left];
     }
-    private:
-        vector<int> psum;    
+private:
+    vector<int> v;
 };
-
 /**
  * Your NumArray object will be instantiated and called as such:
  * NumArray* obj = new NumArray(nums);
- * int param_1 = obj->sumRange(left,right);
+ * 
+ int param_1 = obj->sumRange(left,right);
  */
