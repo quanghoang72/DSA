081c8f1973606c48e36ef91e36142df90f2f849f
quanghoang72 <SUPER_SECRET>
2023-02-22 11:20:20 +0000

    add to lc

--- leetcode/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ leetcode/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
+    //time O(n.log m)              m=sum of vector a
+    //space 0(1)
     int shipWithinDays(vector<int>& a, int k) {
-        int mx=*max_element(a.begin(),a.end());
-        int l=mx;
+        int l=*max_element(a.begin(),a.end());
         int r=accumulate(a.begin(),a.end(),0);
         int res=INT_MAX;
         while(l<=r){
             int m=l+ceil((r-l)*1.0/2);
             int day=1;
             int temp=a[0];
-            int i;
-            for(i=1;i<a.size();i++){
+            for(int i=1;i<a.size();i++){
                 if(temp+a[i]>m){
                     day++;
                     temp=a[i];
