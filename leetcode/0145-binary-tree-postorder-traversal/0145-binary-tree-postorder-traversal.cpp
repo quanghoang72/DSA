03764c514110f515332791a0260ec5a0c10b64d5
quanghoang72 <SUPER_SECRET>
2023-03-21 20:13:05 +0000

    add to lc

--- leetcode/0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ leetcode/0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,17 +11,49 @@
  */
 class Solution {
 public:
+    //recursion
+    // time O(n)
+    // space O(n)
+    // vector<int> postorderTraversal(TreeNode* root) {
+    //     vector<int> res;
+    //     postorder(root,res);
+    //     return res;
+    // }
+    // void postorder(TreeNode* root, vector<int> &res){
+    //     if(!root) return;
+    //     postorder(root->left,res);
+    //     postorder(root->right,res);
+    //     res.push_back(root->val);
+    // }
+
     //time O(n)
     //space O(n)
     vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode*> s;
+        vector<bool> visited;
         vector<int> res;
-        postorder(root,res);
+        s.push(root);
+        visited.push_back(false);
+        while(!s.empty()){
+            auto node=s.top();
+            s.pop();
+            auto v=visited.back();
+            visited.pop_back();
+            if(node){
+                if(!v){
+                    s.push(node);
+                    visited.push_back(true);
+                    s.push(node->right);
+                    visited.push_back(false);
+                    s.push(node->left);
+                    visited.push_back(false);
+                }
+                else{
+                    res.push_back(node->val);
+                }
+            }
+        }
         return res;
     }
-    void postorder(TreeNode* root, vector<int> &res){
-        if(!root) return;
-        postorder(root->left,res);
-        postorder(root->right,res);
-        res.push_back(root->val);
-    }
+
 };
