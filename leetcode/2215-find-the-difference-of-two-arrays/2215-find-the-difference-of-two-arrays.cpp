0f04791dde0d6cb24d9f6e0dbf252566a7d14aaf
quanghoang72 <SUPER_SECRET>
2023-05-03 07:35:14 +0000

    add to lc

--- leetcode/2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ leetcode/2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -7,22 +7,21 @@ public:
         for(auto val : a){
             mp[val]=1;
         }
+        
         for(auto val:b){
             if(mp[val]==0) mp[val]=2;
             else if(mp[val]==1 || mp[val]==-1)
                 mp[val]=-1;
         }
-        vector<vector<int>> res;
-        vector<int> temp;
-        vector<int> temp2;
+        
+        vector<vector<int>> res(2);
+        
         for(auto i:mp){
             if(i.second==1)
-                temp.push_back(i.first);
+                res[0].push_back(i.first);
             else if(i.second==2)
-                temp2.push_back(i.first);
+                res[1].push_back(i.first);
         }
-        res.push_back(temp);
-        res.push_back(temp2);
         return res;
     }
 };
